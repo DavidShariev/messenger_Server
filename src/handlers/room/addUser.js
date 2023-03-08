74908b96ced09f2132a0ec3f2ed67c0b43f0bea2
David <david.shariev.2002@gmail.com>
2023-03-08 23:52:58 +0300

    addUser

---
+++ src/handlers/room/addUser.js
@@ -0,0 +1,11 @@
+const addUser = (req, res) => {
+    try {
+    } catch (err) {
+        console.log(err.message);
+        return res.status(500).json({
+            message: "Ошибка добавления пользователя",
+        });
+    }
+};
+
+export default addUser;
